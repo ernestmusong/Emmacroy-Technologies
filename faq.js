a689ab35f3ddec80ae3737ed7582bfaf11dc302a
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-09-23 20:29:56 +0100

    Fix hamburger on Faqs page

--- faq.js
+++ faq.js
@@ -15,6 +15,37 @@ const accordions = [
     }
   ]
 
+  // SIDE-BAR
+const sideBar = document.querySelector('.sidebar');
+const openBtn = document.querySelector('#open');
+
+openBtn.addEventListener('click', () => {
+  sideBar.classList.toggle('sidebar');
+});
+sideBar.addEventListener('click', () => {
+  sideBar.classList.toggle('sidebar');
+});
+
+// BACK TO TOP
+const navbar = document.getElementById('nav');
+const header = document.getElementById('header');
+const topLink = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = header.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
+  }
+  if (scrollHeight > 300) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
+});
+
 
   const displayAccordions = (arr) => {
     let accordionsContainer = document.querySelector('.accordions')
